c046c2275a70c92098e04a23287e4e50a59d1fc9
unknown <a0134598r@CR2107.iss.nus.edu.sg>
2015-03-02 14:42:05 +0800

    changed to print detailed message

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,10 +1,20 @@
 
+import java.util.*;
+
 public class Greeting {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("good day!");
+		Date d = new Date();
+		
+		if(d.getHours()>6 && d.getHours()<12)
+			System.out.println("good morning!");
+		else if(d.getHours()>12 && d.getHours()<16)
+			System.out.println("good afternoon!");
+		else
+			System.out.println("good evening!");
 
 	}
 
