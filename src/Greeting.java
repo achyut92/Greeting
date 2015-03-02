a1cd269408280185b09864ab5d0e2a69b9c24703
unknown <a0134598r@CR2107.iss.nus.edu.sg>
2015-03-02 15:35:18 +0800

    Print person name with greeting

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,20 +1,29 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Greeting {
 
 	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		String name=" ";
+		BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter your name:");
+		name=b.readLine();
 		
 		Date d = new Date();
 		
 		if(d.getHours()>6 && d.getHours()<12)
-			System.out.println("good morning!");
+			System.out.println("good morning "+name+"!");
 		else if(d.getHours()>12 && d.getHours()<16)
-			System.out.println("good afternoon!");
+			System.out.println("good afternoon "+name+"!");
 		else
-			System.out.println("good evening!");
+			System.out.println("good evening "+name+"!");
 
 	}
 
